cf7ec650bc7459edb8942663fecb2dd793db5dc0
Danilo Hoffmann <hoffmann@evident.nl>
2021-08-22 16:58:50 +0200

    day2

--- ruby/day2/test.rb
+++ ruby/day2/test.rb
@@ -3,6 +3,7 @@ puts '------------------------------'
 # animals = ['lion', 'elephant', 'deer']
 # puts animals
 
+
 # a = []
 # # a[0] = 0
 # # a[1] = 'test'
@@ -16,10 +17,38 @@ puts '------------------------------'
 # puts 'nil' if a.pop.nil?
 # print a, "\n"
 
-numbers = {
-  eins: 'one',
-  zwei: 'two'
-}
 
-eins = :eins
-puts numbers[eins]
+# numbers = {
+#   eins: 'one',
+#   zwei: 'two'
+# }
+
+# eins = :eins
+# puts numbers[eins]
+
+
+# def tell_the_truth(options = {})
+#   if options[:profession] == :lawyer
+#     'it could be believed that this is almost certainly not false.'
+#   else
+#     true
+#   end
+# end
+
+# puts tell_the_truth profession: :lawyer
+# puts tell_the_truth
+
+
+# 3.times { puts '1 hiya there, kiddo' }
+# 3.times do
+#   puts '2 hiya there, kiddo'
+# end
+
+
+# animals = ['lions and', 'tigers and', 'bears', 'oh my']
+# animals.each { |a| print a, ' ' }
+
+
+(1..7).each { |a| puts a % 2 }
+
+

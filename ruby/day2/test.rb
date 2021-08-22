4024d1a20a2d02657da95e524cc6abd81dbd1fa7
Danilo Hoffmann <hoffmann@evident.nl>
2021-08-22 16:28:28 +0200

    day2

--- ruby/day2/test.rb
+++ ruby/day2/test.rb
@@ -1,3 +1,25 @@
-animals = ['lion', 'elephant', 'deer']
+puts '------------------------------'
 
-puts animals
+# animals = ['lion', 'elephant', 'deer']
+# puts animals
+
+# a = []
+# # a[0] = 0
+# # a[1] = 'test'
+# # a[2] = ['hello', 'world']
+# # a.push 123, 456
+# # a.push('1', '2')
+
+# # print a, "\n"
+
+# # https://www.rubydoc.info/gems/rubocop/0.19.0/Rubocop/Cop/Style/NilComparison
+# puts 'nil' if a.pop.nil?
+# print a, "\n"
+
+numbers = {
+  eins: 'one',
+  zwei: 'two'
+}
+
+eins = :eins
+puts numbers[eins]

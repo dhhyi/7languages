3682884946ad36dd0f0a4b40e380ea920ed8295c
Danilo Hoffmann <dhhyi@aol.com>
2022-08-09 15:38:09 +0000

    ruby day2

---
+++ ruby/day2/ss1.rb
@@ -0,0 +1,20 @@
+puts '----------------------------------------'
+
+a = (1..16)
+
+puts a.to_a.inspect
+
+a.each do |i|
+  print i
+  if (i % 4).zero?
+    print "\n"
+  else
+    print ' '
+  end
+end
+
+puts
+
+a.each_slice(4) do |i|
+  puts i.inject { |sum, x| "#{sum} #{x}" }
+end

cb261a2b633ab740771cefa1e6bf02272b6650f3
Danilo Hoffmann <dhhyi@aol.com>
2022-08-18 11:53:13 +0000

    prolog day2

--- prolog/day2/ss5_sort.pl
+++ prolog/day2/ss5_sort.pl
@@ -1,7 +1,7 @@
 % sorted(List, X).
 
+% min_in_list/2 (List, Min)
 min_in_list([Min],Min).
-
 min_in_list([H,K|T],M) :-
     H =< K,
     min_in_list([H|T],M);
@@ -10,20 +10,18 @@ min_in_list([H,K|T],M) :-
 
 % https://stackoverflow.com/questions/60919189/prolog-remove-element-from-list
 
-remover(_, [], []).
-remover(R, [R|T], T2) :- remover(R, T, T2).
-remover(R, [H|T], [H|T2]) :- H \= R, remover(R, T, T2).
-% remove(_, [], []).
-% remove(E, [H|T], T) :- E == H.
+% remove_one/3 (Element, InputList, OutputList)
+remove_one(_, [], []).
+remove_one(R, [R|T], T).
+remove_one(R, [H|T], [H|T2]) :- H \= R, remove_one(R, T, T2).
 
+% sorted/2 (UnsortedList, SortedList)
 sorted([], []).
-% sorted([A], [A]).
-
 sorted(List, [H|Tail]) :-
     % write('at '), write(List), write(', '), write(H), write(', '), write(Tail), nl,
     min_in_list(List, H),
     % write('min in '), write(List), write(' is '), write(H), nl,
-    remover(H, List, Remain),
+    remove_one(H, List, Remain),
     % write('split '), write(List), write(' is '), write(H), write(' and '), write(Remain), nl,
     sorted(Remain, Tail).
 

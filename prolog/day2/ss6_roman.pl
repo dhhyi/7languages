9d2b73866f9c8ce96b1d2c5ddb72eebb253dd77f
Danilo Hoffmann <dhhyi@aol.com>
2022-08-18 14:33:08 +0000

    prolog day2

--- prolog/day2/ss6_roman.pl
+++ prolog/day2/ss6_roman.pl
@@ -1,5 +1,6 @@
 % roman(List, Sum)
 
+roman([], 0).
 roman([i], 1).
 roman([v], 5).
 roman([x], 10).
@@ -9,17 +10,16 @@ roman([d], 500).
 roman([m], 1000).
 
 roman([X|[Y|T]], Sum) :-
-    T \= [],
     roman([X], Xv),
     roman([Y], Yv),
     Yv > Xv,
     roman(T, S2),
-    Sum is Yv - Xv + S2.
-
-roman([X|T], Sum) :-
-    T \= [],
+    Sum is Yv - Xv + S2;
+    roman([X], Xv),
+    roman([Y], Yv),
+    Yv =< Xv,
     roman([X], S1),
-    roman(T, S2),
+    roman([Y|T], S2),
     Sum is S1 + S2.
 
 % roman([c,x,l,i,i],X).

b1ed91aa464035cdfc9542f8a64df72239000c50
Danilo Hoffmann <dhhyi@aol.com>
2022-08-18 20:59:04 +0000

    prolog day3

---
+++ prolog/day3/einstein.pl
@@ -0,0 +1,21 @@
+% There are five houses, each painted a different color. In each house lives a person with a different nationality. These five owners drink a certain type of beverage, smoke a certain brand of cigar, and keep a certain pet. No owners have the same pet, smoke the same cigar, or drink the same beverage.
+
+% The question is, who owns the fish?
+
+% Here are the clues:
+
+% The Brit lives in the red house
+% The Swede keeps dogs as pets
+% The Dane drinks tea
+% The green house is on the left of the white house
+% The green house’s owner drinks coffee
+% The person who smokes Pall Mall rears birds
+% The owner of the yellow house smokes Dunhill
+% The man living in the center house drinks milk
+% The Norwegian lives in the first house
+% The man who smokes blends lives next to the one who keeps cats
+% The man who keeps horses lives next to the man who smokes Dunhill
+% The owner who smokes BlueMaster drinks beer
+% The German smokes Prince
+% The Norwegian lives next to the blue house
+% The man who smokes blend has a neighbor who drinks water
--- prolog/day3/queens_self.pl
+++ prolog/day3/queens_self.pl
@@ -1,12 +1,20 @@
 four_queens(Board) :-
+    member(Y1, [1,2,3,4]),
+    member(Y2, [1,2,3,4]),
+    member(Y3, [1,2,3,4]),
+    member(Y4, [1,2,3,4]),
     Board = [(1, Y1), (2, Y2), (3, Y3), (4, Y4)],
-    fd_domain([Y1, Y2, Y3, Y4], 1, 4),
+    % Rows
     fd_all_different([Y1, Y2, Y3, Y4]),
-    S1 is 1 + Y1, D1 is 1 - Y1,
-    S2 is 2 + Y2, D2 is 2 - Y2,
-    S3 is 3 + Y3, D3 is 3 - Y3,
-    S4 is 4 + Y4, D4 is 4 - Y4,
+    % Diag north-east
+    S1 is 1 + Y1,
+    S2 is 2 + Y2,
+    S3 is 3 + Y3,
+    S4 is 4 + Y4,
     fd_all_different([S1, S2, S3, S4]),
-    fd_all_different([D1, D2, D3, D4]),
-    fd_domain([S1, S2, S3, S4], 2, 8),
-    fd_domain([D1, D2, D3, D4], -3, 3).
+    % Diag south-east
+    D1 is 1 - Y1,
+    D2 is 2 - Y2,
+    D3 is 3 - Y3,
+    D4 is 4 - Y4,
+    fd_all_different([D1, D2, D3, D4]).

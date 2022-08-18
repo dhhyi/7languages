e415f9580cec4af605a92503335a616351b27ef6
Danilo Hoffmann <dhhyi@aol.com>
2022-08-18 19:12:19 +0000

    prolog day3

--- prolog/day3/sudoku.pl
+++ prolog/day3/sudoku.pl
@@ -1,6 +1,3 @@
-valid([]).
-valid([H|T]) :- fd_all_different(H), valid(T).
-
 sudoku(Puzzle, Solution) :-
     Solution = Puzzle,
     Puzzle = [
@@ -10,18 +7,21 @@ sudoku(Puzzle, Solution) :-
         S41, S42, S43, S44
     ],
     fd_domain(Puzzle, 1, 4),
-    Row1 = [S11, S12, S13, S14],
-    Row2 = [S21, S22, S23, S24],
-    Row3 = [S31, S32, S33, S34],
-    Row4 = [S41, S42, S43, S44],
-    Col1 = [S11, S21, S31, S41],
-    Col2 = [S12, S22, S32, S42],
-    Col3 = [S13, S23, S33, S43],
-    Col4 = [S14, S24, S34, S44],
-    Square1 = [S11, S12, S21, S22],
-    Square2 = [S13, S14, S23, S24],
-    Square3 = [S31, S32, S41, S42],
-    Square4 = [S33, S34, S43, S44],
-    valid([Row1, Row2, Row3, Row4, Col1, Col2, Col3, Col4, Square1, Square2, Square3, Square4]).
+    % Rows
+    fd_all_different([S11, S12, S13, S14]),
+    fd_all_different([S21, S22, S23, S24]),
+    fd_all_different([S31, S32, S33, S34]),
+    fd_all_different([S41, S42, S43, S44]),
+    % Columns
+    fd_all_different([S11, S21, S31, S41]),
+    fd_all_different([S12, S22, S32, S42]),
+    fd_all_different([S13, S23, S33, S43]),
+    fd_all_different([S14, S24, S34, S44]),
+    % Squares
+    fd_all_different([S11, S12, S21, S22]),
+    fd_all_different([S13, S14, S23, S24]),
+    fd_all_different([S31, S32, S41, S42]),
+    fd_all_different([S33, S34, S43, S44]).
 
 % sudoku([_,_,2,3, _,_,_,_, _,_,_,_, 3,4,_,_], Solution)
+% sudoku([_,_,4,_, _,1,_,3, _,_,1,_, 1,_,_,4], Solution)

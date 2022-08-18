a5c9e48d22bb4a596f536fa5c9b560c1eebacc41
Danilo Hoffmann <dhhyi@aol.com>
2022-08-18 21:07:53 +0000

    prolog day3

--- prolog/day3/queens_self.pl
+++ prolog/day3/queens_self.pl
@@ -1,20 +1,13 @@
 four_queens(Board) :-
-    member(Y1, [1,2,3,4]),
-    member(Y2, [1,2,3,4]),
-    member(Y3, [1,2,3,4]),
-    member(Y4, [1,2,3,4]),
+    % layout constraints
+    member(Y1, [1,2,3,4]), member(Y2, [1,2,3,4]),
+    member(Y3, [1,2,3,4]), member(Y4, [1,2,3,4]),
     Board = [(1, Y1), (2, Y2), (3, Y3), (4, Y4)],
     % Rows
     fd_all_different([Y1, Y2, Y3, Y4]),
     % Diag north-east
-    S1 is 1 + Y1,
-    S2 is 2 + Y2,
-    S3 is 3 + Y3,
-    S4 is 4 + Y4,
+    S1 is 1 + Y1, S2 is 2 + Y2, S3 is 3 + Y3, S4 is 4 + Y4,
     fd_all_different([S1, S2, S3, S4]),
     % Diag south-east
-    D1 is 1 - Y1,
-    D2 is 2 - Y2,
-    D3 is 3 - Y3,
-    D4 is 4 - Y4,
+    D1 is 1 - Y1, D2 is 2 - Y2, D3 is 3 - Y3, D4 is 4 - Y4,
     fd_all_different([D1, D2, D3, D4]).

5fbcab4490b2bd2bced9ff57085c06e4290c41af
Danilo Hoffmann <dhhyi@aol.com>
2022-08-29 12:46:01 +0000

    erlang day1

--- erlang/day1/ss1_splitstring.erl
+++ erlang/day1/ss1_splitstring.erl
@@ -4,4 +4,5 @@
 % countwords(String) -> length(string:split(String, " ", all)).
 
 countwords([]) -> 0;
-countwords([String]) -> [_|T] = string:split(String, " "), countwords(T) + 1.
+countwords([String]) -> [_|T] = string:split(String, " "), countwords(T) + 1;
+countwords(String) -> countwords([String]).

edffce57c7d2ed2bfa62cb379ff2f19f0e317ef5
Danilo Hoffmann <dhhyi@aol.com>
2022-08-30 12:29:08 +0000

    erlang day2

---
+++ erlang/day2/cases.erl
@@ -0,0 +1,10 @@
+-module(cases).
+-export([start/0]).
+
+start() ->
+    Animal = fox,
+    case Animal of
+        dog -> io:format("Woof~n");
+        cat -> io:format("Meow~n");
+        _ -> io:format("What?~n")
+    end.

74d671483f7359f7b79723ff884777b528ad4000
Danilo Hoffmann <dhhyi@aol.com>
2022-08-30 13:54:43 +0000

    erlang day2

---
+++ erlang/day2/listfunctions.erl
@@ -0,0 +1,26 @@
+-module(listfunctions).
+
+-export([start/0]).
+
+l() -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].
+
+% start() ->
+%   Print = fun(X) -> io:format("~p~n", [X]) end,
+%   lists:foreach(Print, l()).
+
+% start() ->
+%   Print = fun(X) -> io:format("~p~n", [X]) end,
+%   List = lists:map(fun(X) -> X * X end, l()),
+%   lists:foreach(Print, List).
+
+% start() ->
+%   Print = fun(X) -> io:format("~p~n", [X]) end,
+%   Small = fun(X) -> X < 5 end,
+%   List = lists:filter(Small, l()),
+%   lists:foreach(Print, List).
+
+start() ->
+  io:format("~p~n", [l()]),
+  Adder = fun(X, Acc) -> X + Acc end,
+  Sum = lists:foldl(Adder, 0, l()),
+  io:format("Sum is ~p~n", [Sum]).

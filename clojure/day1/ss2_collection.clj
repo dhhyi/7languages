fc29f63b630577a2830ac98d706f442603f0e197
Danilo Hoffmann <dhhyi@aol.com>
2022-09-09 09:25:00 +0000

    clojure day1

--- clojure/day1/ss2_collection.clj
+++ clojure/day1/ss2_collection.clj
@@ -8,10 +8,11 @@
     (vector? coll) :vector
     (set? coll) :set
     (list? coll) :list
-    :else :unknown))
+    :else (class coll)))
 
 (println (collection-type [1 2 3]))
 (println (collection-type #{1 2 3}))
 (println (collection-type {:a 1 :b 2}))
 (println (collection-type '(1 2 3)))
 (println (collection-type "string"))
+(println (collection-type (rest [1 2 3])))

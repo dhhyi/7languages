985b4dd9606ceba2de9743f2b42cfb16cf1ac1bd
Danilo Hoffmann <dhhyi@aol.com>
2022-09-18 11:21:31 +0000

    use runhaskell

--- haskell/.devcontainer/cont.sh
+++ haskell/.devcontainer/cont.sh
@@ -3,6 +3,4 @@
 [ -z "$1" ] && echo "Usage: $0 <script>" && exit 1
 [ ! -f "$1" ] && echo "File not found: $1" && exit 1
 
-trap 'git clean -qXf' INT TERM EXIT
-
-onchange -ik '**/*.hs' -- sh -c "ghc -o executable $1 && ./executable"
+onchange -ik '**/*.hs' -- sh -c "runhaskell $1"

aaa2e06f98e9099d287653c2bcc9b17fd2a91e2a
Danilo Hoffmann <dhhyi@aol.com>
2022-09-19 21:57:01 +0000

    streamline erlang devcontainer

--- erlang/.devcontainer/cont.sh
+++ erlang/.devcontainer/cont.sh
@@ -7,4 +7,6 @@ file=`basename $1 .erl`
 
 echo "using $file in $dir"
 
+trap "git clean -Xf" EXIT INT TERM
+
 onchange --await-write-finish 500 -ik "$dir/**" -- sh -c "echo 'compiling...' && erlc $dir/$file.erl && echo '--------------' && erl -noshell -s $file start -s init stop"

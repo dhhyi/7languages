c94fa4e89af7569ddbfac04d6adbfa20823b5f1d
Danilo Hoffmann <dhhyi@aol.com>
2022-09-20 15:40:45 +0000

    haskell day2

--- haskell/day2/ss7_text.hs
+++ haskell/day2/ss7_text.hs
@@ -1,7 +1,5 @@
 module Main where
 
-import Data.Text (justifyRight)
-
 loremIpsum =
   "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
 
@@ -33,7 +31,7 @@ alignedLeft :: [String] -> String
 alignedLeft = concatenate
 
 alignedRight :: Int -> [String] -> String
-alignedRight n words = do
+alignedRight n words =
   replicate space ' ' ++ concatenate words
   where
     space = n - length (concat words) - length words + 1

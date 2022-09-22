d7caf0dc57af07ad48fbf6e046f9667ede834594
Danilo Hoffmann <dhhyi@aol.com>
2022-09-22 09:28:39 +0200

    lua devcontainer

---
+++ lua/.devcontainer/cont.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+file="$1"
+
+[ -z "$file" ] && echo "Usage: $0 <file>" && exit 1
+[ ! -f "$file" ] && echo "File not found: $file" && exit 1
+
+run () {
+    lua "$file"
+}
+
+run
+while inotifywait -qq -e close_write "$file"; do run; done
---
+++ lua/.devcontainer/disclaimer.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+(
+    set -x
+    lua -v
+)
+
+echo
+cat <<EOF
+Use the command "lua" to start a REPL.
+Use the command "cont <script>" to continuously evaluate a given script.
+
+Alternatively you can use the VSCode tasks.
+EOF

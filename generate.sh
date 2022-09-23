3bd56c8628b38f748cfb734dc404bf61b4f46851
Danilo Hoffmann <dhhyi@aol.com>
2022-09-23 14:51:53 +0200

    use pipefail

--- generate.sh
+++ generate.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+MYSHELL=`ps -hp $$ | awk '{print $5}'`
+if [ "$(basename $MYSHELL)" != "bash" ]
+then
+    echo "script it written for bash only, detected $MYSHELL"
+    exit 1
+fi
 
 set -e
+set -o pipefail
 
 [ -z "$1" ] && echo "Usage: $0 <folder>" && exit 1
 [ ! -d "$1" ] && echo "Error: $1 is not a folder" && exit 1
@@ -12,6 +20,6 @@ docker inspect "$GOMPLATE_IMAGE" >/dev/null || docker pull "$GOMPLATE_IMAGE"
 
 cat .templates/generate.sh.gomplate | docker run --rm -i -e FOLDER=$1 -v $PWD/$1/language.yaml:/language.yaml:ro $GOMPLATE_IMAGE -d language=/language.yaml -f - > $1/generate.sh~
 
-sh $1/generate.sh~
+bash $1/generate.sh~
 
 echo "Done"

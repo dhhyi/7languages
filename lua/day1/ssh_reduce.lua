ebc825ad02b7ec0a9b0e6210180c3126ec9f23a3
Danilo Hoffmann <dhhyi@aol.com>
2022-09-23 16:01:33 +0000

    lua day1

--- lua/day1/ssh_reduce.lua
+++ lua/day1/ssh_reduce.lua
@@ -27,3 +27,15 @@ end
 
 print(factorial(5))
 print(1 * 2 * 3 * 4 * 5)
+
+print "-- fibonacci"
+
+function fib_next_pair(arr)
+    return { arr[2], arr[1] + arr[2] }
+end
+
+function fib(n)
+    return reduce(n - 1, { 0, 1 }, fib_next_pair)[2]
+end
+
+print(fib(5))
